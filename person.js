aa1fe77e3f039047d29ee1f7ab81b80d15b720ab
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-09-11 14:43:29 -0500

    #6 ex. incomplete
    not sure how to get random number

--- person.js
+++ person.js
@@ -1,5 +1,16 @@
 class Person {
+  #id; 
   // Write code here
+constructor(id) {
+
+this.#id = id;
 }
+getNum() {
+  return Math.random * this.#id;
+}
+
+}
+const person = new Person()
+console.log(person.getNum(Math.random()));
 
 module.exports = Person;
